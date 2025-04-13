7be2871f1e4dbd669b6452d202353013f1f080f7
chinedunewbirth <chineduchukwuemekamazi@gmail.com>
2025-04-13 21:50:23 +0100

    include test_app.py 3

--- test_app.py
+++ test_app.py
@@ -1,19 +1,16 @@
-# test_app.py
 import unittest
 from app import app
 
-class FlaskAppTests(unittest.TestCase):
+class FlaskAppTestCase(unittest.TestCase):
     def setUp(self):
-        # Set up test client
-        self.app = app.test_client()
-        self.app.testing = True
+        self.client = app.test_client()
 
     def test_home_status_code(self):
-        response = self.app.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_home_content(self):
-        response = self.app.get('/')
+        response = self.client.get('/')
         self.assertIn(b'Hello from Flask CI/CD!', response.data)
 
 if __name__ == '__main__':
